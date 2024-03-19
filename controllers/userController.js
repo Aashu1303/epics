e449f0e7f460ace120dfa1655402eb1796c08e3d
Aashu1303 <aashutoshag03@gmail.com>
2024-03-19 22:59:39 +0530

    Resolving bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,8 @@ const userController = {};
 
 userController.getUserProfile = async (req, res) => {
   try {
-    const userId = req.user._id; // Assuming user ID is accessible from the middleware
-
+    const userId = req.user.userId; // Assuming user ID is accessible from the middleware
+    console.log(userId);
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
