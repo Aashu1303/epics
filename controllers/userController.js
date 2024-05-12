1d500e27a957c88d11f3cf1b4f10977f58606a02
Sahil Sundrani <sahilsundrani@Sahils-MacBook-Air.local>
2024-05-12 14:19:18 +0530

    updated user-controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,51 +1,69 @@
 const User = require('../models/User');
+const Admin = require('../models/Admin');
 
 const userController = {};
 
 userController.getUserProfile = async (req, res) => {
   try {
-    const userId = req.user.userId; // Assuming user ID is accessible from the middleware
-
+    const userId = req.user.userId;
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-
-    const userProfile = {
-      username: user.username,
-      email: user.email,
-    };
-
-    res.json(userProfile);
+    user.password = undefined;
+    return res.status(200).json(user);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+userController.getAdminProfile = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const admin = await Admin.findById(userId);
+    if (!admin) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    admin.password = undefined;
+    return res.status(200).json(admin);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
 userController.updateUserProfile = async (req, res) => {
   try {
-    const userId = req.user._id; // Assuming user ID is accessible from the middleware
+    const userId = req.user._id; 
     const updates = req.body;
 
-    // Validate user input (implement validation logic based on allowed updates)
-
-    const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true }); // Returns the updated document
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true }); 
     if (!updatedUser) {
       return res.status(404).json({ message: 'User not found' });
     }
-
-    // Select specific user information to expose (e.g., exclude password)
-    const updatedProfile = {
-      username: updatedUser.username,
-      email: updatedUser.email,
-      // Add other relevant user profile details
-    };
-
-    res.json(updatedProfile);
+    updatedUser.password = undefined;
+    return res.json(updatedUser);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+userController.updateAdminProfile = async (req, res) => {
+  try {
+    const userId = req.user._id; 
+    const updates = req.body;
+
+    const updatedUser = await Admin.findByIdAndUpdate(userId, updates, { new: true }); 
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    updatedUser.password = undefined;
+    return res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const ensureAuthenticated = require('../middlewares/auth'); // Assuming you have an authentication middleware
+const ensureAuthenticated = require('../middlewares/auth');
 
-router.get('/me', ensureAuthenticated, userController.getUserProfile);
+router.get('/user-profile', ensureAuthenticated, userController.getUserProfile);
+router.get('/admin-profile', ensureAuthenticated, userController.getAdminProfile);
 
-router.put('/me', ensureAuthenticated, userController.updateUserProfile);
+router.get('/update-user', ensureAuthenticated, userController.updateUserProfile);
+router.get('/update-admin', ensureAuthenticated, userController.updateAdminProfile);
 
 module.exports = router;
